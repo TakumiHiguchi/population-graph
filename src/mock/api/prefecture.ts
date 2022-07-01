1e1f23f3ad85c75e635c08d5b84080bb816064ca
TakumiHiguchi <andf.for.gt@gmail.com>
2022-07-02 08:43:49 +0900

    feat: apiテストのためにmockサーバーを構築

---
+++ src/mock/api/prefecture.ts
@@ -0,0 +1,28 @@
+import { MockedRequest, ResponseResolver, restContext } from 'msw';
+
+export const prefectures: ResponseResolver<MockedRequest, typeof restContext> =
+	(req, res, ctx) => {
+		if (
+			!req.headers.get('X-API-KEY') ||
+			req.headers.get('X-API-KEY') == 'error'
+		) {
+			return res(
+				ctx.status(200),
+				ctx.json({ statusCode: '403', message: 'Forbidden.', description: '' }),
+			);
+		}
+		return res(
+			ctx.status(200),
+			ctx.json({
+				message: '',
+				result: [
+					{ prefCode: 2, prefName: '青森県' },
+					{ prefCode: 3, prefName: '岩手県' },
+					{ prefCode: 4, prefName: '宮城県' },
+					{ prefCode: 5, prefName: '秋田県' },
+					{ prefCode: 46, prefName: '鹿児島県' },
+					{ prefCode: 47, prefName: '沖縄県' },
+				],
+			}),
+		);
+	};
--- src/mock/handler.ts
+++ src/mock/handler.ts
@@ -1,1 +1,6 @@
-export const handlers = [];
+import { rest } from 'msw';
+import { prefectures } from './api/prefecture';
+
+export const handlers = [
+	rest.get(process.env.RESAS_ENDPOINT + '/api/v1/prefectures', prefectures),
+];
