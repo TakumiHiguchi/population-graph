c5b2c6e411f336c618fa5592d33e10af755a3571
TakumiHiguchi <andf.for.gt@gmail.com>
2022-07-02 03:14:32 +0900

    feat: jestの導入

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+	dir: './',
+});
+
+const customJestConfig = {
+	moduleDirectories: ['node_modules', '<rootDir>/'],
+	testEnvironment: 'jest-environment-jsdom',
+	setupFilesAfterEnv: ['./test/jest.setup.ts'],
+	moduleNameMapper: {
+		'^components/(.*)$': '<rootDir>/src/components/$1',
+		'^pages/(.*)$': '<rootDir>/src/pages/$1',
+		'^utils/(.*)$': '<rootDir>/src/utils/$1',
+		'^hooks/(.*)$': '<rootDir>/src/hooks/$1',
+		'^types/(.*)$': '<rootDir>/src/types/$1',
+		'^lib/(.*)$': '<rootDir>/src/lib/$1',
+		'^mock/(.*)$': '<rootDir>/src/mock/$1',
+		'^styles': '<rootDir>/src/styles',
+	},
+};
+
+module.exports = createJestConfig(customJestConfig);
