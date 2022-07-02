fdf9d91f1c7194cbe591058a4be21f5730031ae4
TakumiHiguchi <andf.for.gt@gmail.com>
2022-07-03 04:03:17 +0900

    feat: secondary buttonコンポーネントを追加

---
+++ src/components/molecules/Button/Secondary/styles.ts
@@ -0,0 +1,8 @@
+import { backGroundColor, borderRadiusSize, textColor } from 'styles';
+
+export const buttonSecondary = `
+  background: ${backGroundColor.primary};
+  border-radius: ${borderRadiusSize.pc.verySmall};
+  border: 1px solid ${backGroundColor.pastel.bluePurple.bluePurple15};
+  color: ${textColor.primary};
+`;
