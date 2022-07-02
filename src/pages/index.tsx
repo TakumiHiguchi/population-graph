290926705cbaad4f28a7568e18063f1a649bb5e3
TakumiHiguchi <andf.for.gt@gmail.com>
2022-07-02 21:47:50 +0900

    feat: 都道府県選択サイドバーを含めたpagesの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
 import type { NextPage } from 'next';
+import Sidebar from 'components/organisms/Sidebar';
+import { usePrefecturesApi } from 'hooks/api/prefecture/usePrefecturesApi';
+import DefaultTemplate from 'components/templates/Default';
 
 const Home: NextPage = () => {
-	return <div></div>;
+	const apiKey = process.env.NEXT_PUBLIC_API_KEY
+		? process.env.NEXT_PUBLIC_API_KEY
+		: '';
+	const prefectureApi = usePrefecturesApi(apiKey);
+
+	return (
+		<DefaultTemplate>
+			<div></div>
+			{!prefectureApi.loading && !prefectureApi.isError && (
+				<Sidebar
+					handleChangeCheckBoxState={(prefectureIds: number[]) =>
+						console.log(prefectureIds)
+					}
+					prefectures={prefectureApi.data}
+				/>
+			)}
+		</DefaultTemplate>
+	);
 };
 
 export default Home;
