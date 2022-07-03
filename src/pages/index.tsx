ba9b763770e5d1b5e71498e86b44553c3f6d8b26
TakumiHiguchi <andf.for.gt@gmail.com>
2022-07-03 22:38:32 +0900

    feat: pages/indexにheadに含めるmetaタグを表示するコンポーネントを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Chart from 'components/organisms/Chart';
 import Sidebar from 'components/organisms/Sidebar';
 import { usePrefecturesApi } from 'hooks/api/prefecture/usePrefecturesApi';
 import { usePopulaionDatas } from '../hooks/pages/usePopulationDatas';
+import Head from 'components/molecules/Head';
 
 const Home: NextPage = () => {
 	const apiKey = process.env.NEXT_PUBLIC_API_KEY
@@ -14,6 +15,12 @@ const Home: NextPage = () => {
 
 	return (
 		<DefaultTemplate>
+			<Head
+				title='都道府県別総人口推移グラフ'
+				description='総人口推移グラフ表示サイトです。'
+				image='/img/ogimage.webp'
+				url='https://population-graph.branchwith.jp'
+			/>
 			<Chart
 				populations={populationApi.data}
 				prefectures={prefectureApi.data}
