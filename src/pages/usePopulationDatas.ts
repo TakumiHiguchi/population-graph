fa0fdcc04dcd479149b62ad03aee74f183205017
TakumiHiguchi <andf.for.gt@gmail.com>
2022-07-03 10:12:26 +0900

    feat: 人口のデータを管理するカスタムフックを作成

---
+++ src/pages/usePopulationDatas.ts
@@ -0,0 +1,60 @@
+import { removeResasAPIResponseType } from 'lib/api';
+import { getArraysDiff } from 'lib/array';
+import { generateCacheKey } from 'lib/cacheKey';
+import { useState } from 'react';
+import { useSWRConfig } from 'swr';
+import { PopulationCompositionType } from 'types';
+import { usePopulaionApi } from 'hooks/api/population/usePopulationApi';
+
+export const usePopulaionDatas = (apiKey: string) => {
+	const { cache } = useSWRConfig();
+	const [noChachedPrefectureIds, setNoChachedPrefectureIds] = useState<
+		number[]
+	>([]);
+	const [chachedPrefectureIds, setChachedPrefectureIds] = useState<number[]>(
+		[],
+	);
+	const { data, loading, isError } = usePopulaionApi(
+		apiKey,
+		noChachedPrefectureIds,
+	);
+
+	const chachedPopulationDatas =
+		removeResasAPIResponseType<PopulationCompositionType>(
+			chachedPrefectureIds.map((prefCode) =>
+				cache.get(generateCacheKey('population', apiKey, prefCode)),
+			),
+		);
+
+	const shapedChachePopulationDatas = chachedPopulationDatas.filter(
+		(d) => typeof Object.values(d)[0] != 'undefined',
+	);
+
+	const setPopulationDatas = (prefectureIds: number[]) => {
+		const noChachedIds = prefectureIds.filter(
+			(prefCode) =>
+				!cache.get(generateCacheKey('population', apiKey, prefCode)),
+		);
+		const chachedIds = getArraysDiff(prefectureIds, noChachedIds);
+
+		setNoChachedPrefectureIds(noChachedIds);
+		setChachedPrefectureIds(chachedIds);
+	};
+
+	const returnDatas = [
+		...removeResasAPIResponseType(data),
+		...shapedChachePopulationDatas,
+	];
+
+	const actions = {
+		setPopulationDatas,
+	};
+
+	return {
+		data: returnDatas,
+		loading,
+		isError,
+		nowLoadingCount: noChachedPrefectureIds.length,
+		actions,
+	};
+};
