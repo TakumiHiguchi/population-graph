f4f255141a892dc63862fd27d141e01c1a46e916
TakumiHiguchi <andf.for.gt@gmail.com>
2022-07-03 13:15:10 +0900

    feat: prefecturemockに北海道を追加

--- src/mock/api/prefecture.ts
+++ src/mock/api/prefecture.ts
@@ -16,6 +16,7 @@ export const prefectures: ResponseResolver<MockedRequest, typeof restContext> =
 			ctx.json({
 				message: '',
 				result: [
+					{ prefCode: 1, prefName: '北海道' },
 					{ prefCode: 2, prefName: '青森県' },
 					{ prefCode: 3, prefName: '岩手県' },
 					{ prefCode: 4, prefName: '宮城県' },
